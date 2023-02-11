47664881b07704af858c3faf07a9ecc591a29587
Siro_256 <siro@siro256.dev>
2023-02-12 06:18:47 +0900

    Create mod class

--- src/main/java/dev/siro256/forgemod/expandmaxcollision/ExpandMaxCollision.java
+++ src/main/java/dev/siro256/forgemod/expandmaxcollision/ExpandMaxCollision.java
@@ -1,4 +1,6 @@
 package dev.siro256.forgemod.expandmaxcollision;
 
+import net.minecraftforge.fml.common.Mod;
 
+@Mod(modid = "@modId@", name = "@modName@", version = "@modVersion@")
 public class ExpandMaxCollision {}
