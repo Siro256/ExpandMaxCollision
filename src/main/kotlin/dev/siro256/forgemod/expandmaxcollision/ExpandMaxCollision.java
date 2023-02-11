bd9981007b7aa623f61b01fee065dae519297688
Siro_256 <siro@siro256.dev>
2023-02-12 00:14:41 +0900

    Create main class

---
+++ src/main/kotlin/dev/siro256/forgemod/expandmaxcollision/ExpandMaxCollision.java
@@ -0,0 +1,42 @@
+package dev.siro256.forgemod.expandmaxcollision;
+
+import net.minecraftforge.common.ForgeVersion;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+import java.util.Map;
+
+@IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
+public class ExpandMaxCollision implements IFMLLoadingPlugin {
+    static {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("expandmaxcollision.mixins.json");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
